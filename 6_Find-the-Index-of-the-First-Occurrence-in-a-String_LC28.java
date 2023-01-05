f91919a597c6feec2b1a910b024ce5e80bda392e
Raushan Kumar Jha <jha.rau@northeastern.edu>
2023-01-04 23:00:43 -0500

    Create 6_Find-the-Index-of-the-First-Occurrence-in-a-String_LC28.java

---
+++ 6_Find-the-Index-of-the-First-Occurrence-in-a-String_LC28.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        //Approach1 -- TC:O(M*N) SC:O(1)
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            for(int j=0;j<needle.length();j++){
+                if(haystack.charAt(i+j) != needle.charAt(j)){
+                    break;
+                }
+                if(j==needle.length()-1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+        
+    }
+}
